2766621b03a4434f8d72f2f58d6a90b257b5c362
sauduf <saudulfaisal5@gmail.com>
2025-11-11 02:38:54 +0000

    Added Bootstrap styling to sneaker list table

--- list-sneakers.php
+++ list-sneakers.php
@@ -1,20 +1,23 @@
 <!doctype html>
 <html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
 <body>
     <h1>Welcome to my Sneakers Collection</h1>
 
     <a href="add-sneaker-form.php" class="btn btn-primary">Add a Sneaker</a>
-	
+    
     <?php
-	// Connect to database
+    // Connect to database
     include("db.php");
-	
-	// Run SQL Query
+    
+    // Run SQL Query
     $sql = "SELECT * FROM sneakers ORDER BY release_date";
     $results = mysqli_query($mysqli, $sql);
     ?>
 
-    <table>
+    <table class="table table-striped table-hover">
         <?php while($row = mysqli_fetch_assoc($results)): ?>
         <tr>
             <td><?= $row['brand'] ?></td>
@@ -26,3 +29,4 @@
 
 </body>
 </html>
+
