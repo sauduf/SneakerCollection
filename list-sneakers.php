b1494fef842326999548bf8bbb48172d91de2176
sauduf <saudulfaisal5@gmail.com>
2025-11-11 01:48:55 +0000

    Added list-sneakers.php to display sneakers ordered by release date

---
+++ list-sneakers.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<body>
+    <h1>Welcome to my Sneakers Collection</h1>
+
+    <a href="add-sneaker-form.php" class="btn btn-primary">Add a Sneaker</a>
+	
+    <?php
+	// Connect to database
+    include("db.php");
+	
+	// Run SQL Query
+    $sql = "SELECT * FROM sneakers ORDER BY release_date";
+    $results = mysqli_query($mysqli, $sql);
+    ?>
+
+    <table>
+        <?php while($row = mysqli_fetch_assoc($results)): ?>
+        <tr>
+            <td><?= $row['brand'] ?></td>
+            <td><?= $row['name'] ?></td>
+            <td><?= $row['price'] ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+
+</body>
+</html>
