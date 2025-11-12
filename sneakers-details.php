3acd070e878dbd32964dd1638e25fa718cd5b56c
sauduf <saudulfaisal5@gmail.com>
2025-11-12 00:51:10 +0000

    Add sneakers-details.php to display single sneaker information using ?id=

---
+++ sneakers-details.php
@@ -0,0 +1,23 @@
+<?php
+// Connect to database and run SQl query
+include("db.php");
+
+// Grab id value from URL
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM sneakers WHERE sneaker_id = {$id}";
+$rst = mysqli_query($mysqli, $sql);
+$row = mysqli_fetch_assoc($rst);
+?>
+
+<h1><?= $row['brand'] . " " . $row['name'] ?></h1>
+
+<p>
+    <strong>Release Date:</strong> <?= $row['release_date'] ?><br>
+    <strong>Price:</strong> £<?= $row['price'] ?><br>
+    <strong>Condition:</strong> <?= $row['shoecondition'] ?><br>
+    <strong>Color:</strong> <?= $row['color'] ?><br>
+    <strong>Size:</strong> <?= $row['size'] ?>
+</p>
+
+<a href="list-sneakers.php">&lt;&lt; Back to list</a>
